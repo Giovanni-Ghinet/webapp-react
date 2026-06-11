0b4ff9e12b7ed7ef4425de54350338219322b3ea
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-06-11 18:42:44 +0200

    corretto errore key

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -30,14 +30,10 @@ function ProductList() {
 
   return (
     <ul>
-      {products.map(product => (<>
+      {products.map(product => (
         <li key={product.id}>
             {product.image} - {product.price} € - {product.description} - {product.name} - {product.country}
-        </li>
-        <div>
-          <img src={product.image} alt={product.name} />
-        </div>
-        </>
+        </li> 
       ))}
     </ul>
   );
