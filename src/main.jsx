c84a6607d26cfa398e84c31a9e77c58dbee85f3a
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-06-12 18:40:50 +0200

    creato il carosello del menù

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 // Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap Icons CSS
+import 'bootstrap-icons/font/bootstrap-icons.css'
 // Bootstrap JS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
